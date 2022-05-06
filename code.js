155a394b9ecaafbb9c31573c9b1e7b10116536d1
Francisco Cortez <franciscocortezdev@gmail.com>
2022-05-06 12:56:54 -0600

    Timers done

--- code.js
+++ code.js
@@ -2,13 +2,43 @@ const pMin = document.querySelector('.pomo_min');
 const pSec = document.querySelector('.pomo_sec');
 const btnStart = document.querySelector('.inicio');
 const btnStop = document.querySelector('.parar');
+const btnPomo = document.querySelector('.pomos');
+const pomoStart = document.querySelector('.start');
+const pomoRest = document.querySelector('.rest');
+const pomoRestL = document.querySelector('.restLong');
+
+
 let sec = 60;
 let min = 25;
 onTimer = false
 
 
-btnStart.addEventListener('click', ()=>{
-  if(onTimer){
+btnPomo.addEventListener('click', (e)=>{
+  console.log(e.target);
+
+  let btn = e.target.innerHTML;
+  if (btn == 'Pomodoro'){
+    pomoStart.classList.add('active')
+    pomoRest.classList.remove('active')
+    pomoRestL.classList.remove('active')
+  }else if(btn == 'Descanso'){
+    pomoRest.classList.add('active')
+    pomoStart.classList.remove('active')
+    pomoRestL.classList.remove('active')
+  }else if(btn == 'Descanso largo'){
+    pomoRestL.classList.add('active')
+    pomoStart.classList.remove('active')
+    pomoRest.classList.remove('active')
+  }
+
+
+
+})
+
+
+
+btnStart.addEventListener('click', ()=>{       
+  if(onTimer){ 
     onTimer = false;
     clearInterval(pomo);
     btnStart.innerHTML = 'Continuar';
@@ -43,7 +73,6 @@ const counter = ()=>{
         clearInterval(pomo)
         return  console.log('terminado');
       }else if (sec == 0){
-        
           sec = 60;
           min --;
         
@@ -52,7 +81,7 @@ const counter = ()=>{
     sec --
     pMin.innerHTML = min;
     pSec.innerHTML = sec;
-    console.log(`minuto: ${min} segundo: ${sec}`)
+    
     
   }, 1000);
 
@@ -73,4 +102,3 @@ const stopAll = ()=>{
 }
 
 
-// clearInterval(pomo)
